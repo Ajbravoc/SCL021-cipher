fad507d31c773aebeb8816d407bb1f1408416fb2
Ajbravoc <ajbravoc@gmail.com>
2022-07-13 13:34:36 -0400

    V.13.07

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,27 @@
 const cipher = {
-  // ...
-};
+  
+  cifrar:(texto, desplazamiento) =>{
+    if (!texto)
+    return "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    desplazamiento = (desplazamiento % 26 + 26) %26;
+    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26]);
+    
+},  
 
+
+ descifrar: (texto,desplazamiento)=>{
+  if (!texto)
+  return "";
+  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  desplazamiento = (desplazamiento % 26 + 26) % 26;
+  return texto.replace (/[A-Z]/ig, c=> [(letras.indexOf(c)+desplazamiento)%26]);
+
+}
+
+}
 
 
 export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,64 +1,43 @@
-const alphabet = [
-    'A','B','C','D','E','F',
-    'G','H','I','J','K','L',
-    'M','N','O','P','Q','R',
-    'S','T','U','V','W','X',
-    'Y','Z' 
-];
-  
-function encryptText() {
+import cipher from './cipher.js';
 
-const form = document.forms[0];
+window.addEventListener("load", inicio, true);
 
-let title=
- document.getElementById("titleId");  
-   
-title.innerHTML = "Encrypted text";
+function inicio(){
+    document.getElementById("PasswordText").addEventListener("keyup", function(){
+        this.value = this.value.toUpperCase(); //aca le estamos dando la funcion de poner en mayusculas el mensaje que se esta ingresando
+    },true);
 
-let shift= Number(form.shift.value); 
-   
-let pass =  
-  form.pass.value;       
-   
-form.pass.value 
-  = [... pass ].map(char =>
-    encrypt(char, shift)).join('');
+
+document.getElementById("BtnCipher").addEventListener("click", function(){
+    let texto = document.getElementById("PasswordText").value;
+    let desplazamiento = document.getElementById("desplazamiento").value;
+document.getElementById("PasswordCipher").value = cipher.cifrar(texto, desplazamiento);
+}, true);
 }
-  
-function decryptText() {
-const form = document.forms[0];
-let title = document.getElementById("titleId");       
 
-title.innerHTML = "Plain text";
-     
-let shift =   
-  Number(form.shift.value);
-let pass = 
-  form.pass.value;    
-   
-shift = 
-   (alphabet.length - shift) %  
-    alphabet.length;
+function seleccionarTexto() { //Funcion con nombre seleccionar 
+    let desde = PasswordCipher.selectionStart; //Desde representa el inicio del PasswordCipher
+    let hasta = PasswordCipher.selectionEnd; //Hasta representa el fin del PasswordCipher
+    let sel = PasswordCipher.value.substring(desde, hasta); // Selecciona los caracteres del PasswordCipher
+      //Substring extrae caracteres de una cadena entre dos índices especificado)
   
-form.pass.value 
-    = [... pass ].map(char => 
-      encrypt(char,    
-      shift)).join('');
-}
-  
-function encrypt(char, shift) {
-let include =        
- alphabet.includes(
-  char.toUpperCase()); 
-   
-if (include){      
- let position =         
-  alphabet.indexOf(
-   char.toUpperCase());
-    
-let newPosition = 
-  (position+shift) %  
-    alphabet.length;
-return alphabet[newPosition];
-}else  return char;
-}        
+    if (sel.length > 0) {// Si hay algo seleccionado mayor a 0, considerando que se cuenta desde el 0
+      return sel; //retorna todo lo sel
+    } else { // de lo contrario selecciona todo el texto
+      return PasswordCipher.select();
+    }
+  }
+
+  BtnCopy.addEventListener("click", () => {
+    seleccionarTexto();
+    try {
+      let exito = document.execCommand("copy"); // devuelve true o false
+      /*let msg = exito ? "\xe9xito" : "error";
+      console.log("Hay " + msg);*/
+    } catch (error) {
+      let err = error;
+      /*console.log("No fue posible copiar el texto seleccionado!");*/
+    }
+  });
+
+console.log(cipher);
