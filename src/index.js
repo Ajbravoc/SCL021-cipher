bbc630e76182b80bb7202f7eaba8b269c786948c
Ajbravoc <ajbravoc@gmail.com>
2022-07-15 15:21:15 -0400

    V.15.07

--- src/index.js
+++ src/index.js
@@ -3,41 +3,23 @@ import cipher from './cipher.js';
 window.addEventListener("load", inicio, true);
 
 function inicio(){
-    document.getElementById("PasswordText").addEventListener("keyup", function(){
-        this.value = this.value.toUpperCase(); //aca le estamos dando la funcion de poner en mayusculas el mensaje que se esta ingresando
+    document.getElementById("PasswordText").addEventListener("keyup", function(){ //Llama al Id Passwordtext desde el html//
+        this.value = this.value.toUpperCase(); //Le estamos dando la funcion de poner en mayusculas el mensaje que se esta ingresando
     },true);
 
 
-document.getElementById("BtnCipher").addEventListener("click", function(){
-    let texto = document.getElementById("PasswordText").value;
-    let desplazamiento = document.getElementById("desplazamiento").value;
-document.getElementById("PasswordCipher").value = cipher.cifrar(texto, desplazamiento);
+document.getElementById("BtnCipher").addEventListener("click", function(){ //LLama a BtnCipher//
+    let texto = document.getElementById("PasswordText").value; //Llama al Passwordtext//
+    let desplazamiento = document.getElementById("desplazamiento").value; //Llama al desplazamiento//
+document.getElementById("PasswordCipher").value = cipher.cifrar(texto, desplazamiento); //Llama al PasswordCipher donde llama a la const cipher.cifrar para que desplace//
 }, true);
 }
 
-function seleccionarTexto() { //Funcion con nombre seleccionar 
-    let desde = PasswordCipher.selectionStart; //Desde representa el inicio del PasswordCipher
-    let hasta = PasswordCipher.selectionEnd; //Hasta representa el fin del PasswordCipher
-    let sel = PasswordCipher.value.substring(desde, hasta); // Selecciona los caracteres del PasswordCipher
-      //Substring extrae caracteres de una cadena entre dos índices especificado)
+ 
+    document.getElementById( "BtnCopy" ).addEventListener( "click" , () =>{
+    PasswordCipher.select(); //Select hace focus al campo y el copy lo mueve al portapapeles
+    document.execCommand("copy"); //   
+      alert("Tu mensaje ha sido copiado!");
+  });  
   
-    if (sel.length > 0) {// Si hay algo seleccionado mayor a 0, considerando que se cuenta desde el 0
-      return sel; //retorna todo lo sel
-    } else { // de lo contrario selecciona todo el texto
-      return PasswordCipher.select();
-    }
-  }
-
-  BtnCopy.addEventListener("click", () => {
-    seleccionarTexto();
-    try {
-      let exito = document.execCommand("copy"); // devuelve true o false
-      /*let msg = exito ? "\xe9xito" : "error";
-      console.log("Hay " + msg);*/
-    } catch (error) {
-      let err = error;
-      /*console.log("No fue posible copiar el texto seleccionado!");*/
-    }
-  });
-
 console.log(cipher);
