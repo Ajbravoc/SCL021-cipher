250d6e90f12e6c5799da44bd9eb0cbd51cb9e8ed
Ajbravoc <ajbravoc@gmail.com>
2022-07-18 09:59:45 -0400

    V. 18.07

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,15 @@
 const cipher = {
   
-  cifrar:(texto, desplazamiento) =>{
-    if (!texto)
+  cifrar:(texto, desplazamiento) =>{ //propiedad cifrar tiene los parámetros texto y despla//
+    if (!texto) //!not texto, si no hay texto, retorna nada//
     return "";
     const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    desplazamiento = (desplazamiento % 26 + 26) %26;
-    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26]);
-    
-},  
-
-
- descifrar: (texto,desplazamiento)=>{
-  if (!texto)
-  return "";
-  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  desplazamiento = (desplazamiento % 26 + 26) % 26;
-  return texto.replace (/[A-Z]/ig, c=> [(letras.indexOf(c)+desplazamiento)%26]);
+    desplazamiento = (desplazamiento % 26 + 26) %26; //fórmula cipher para desplazarse, 26 letras del alfabeto
+    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26]); //i= no distingue ni lower ni uppercase, g=busca todas las coincidencias)
+},  // función flecha => alternativa compacta a una expresión de función 
+// IndexOf retorna el primer índice que se puede encontrar en un elemento dado //
 
 }
-
-}
-
 
 export default cipher;
 
