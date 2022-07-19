be715b47b0025577f29c7f637cb91bae1952d37f
Ajbravoc <ajbravoc@gmail.com>
2022-07-19 10:53:53 -0400

    elimine codigo sobrante

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,18 @@
 const cipher = {
   
   cifrar:(texto, desplazamiento) =>{ //propiedad cifrar tiene los parámetros texto y despla//
-    if (!texto) //!not texto, si no hay texto, retorna nada//
-    return "";
     const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     desplazamiento = (desplazamiento % 26 + 26) %26; //fórmula cipher para desplazarse, 26 letras del alfabeto
-    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26]); //i= no distingue ni lower ni uppercase, g=busca todas las coincidencias)
-},  // función flecha => alternativa compacta a una expresión de función 
-// IndexOf retorna el primer índice que se puede encontrar en un elemento dado //
-
+    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26]);    
 }
+}
+//se toman todas las coincidencias en texto.replace de la A-Z
+/*se le aplica la función de la c, indexOf saca la posición de cada letra del array letras y se suma desplz
+retornado el valor con el despl. modificado*/
+
+//i= no distingue ni lower ni uppercase, g=busca todas las coincidencias
+// función flecha => alternativa compacta a una expresión de función 
+// IndexOf retorna el primer índice que se puede encontrar en un elemento dado //
 
 export default cipher;
 
