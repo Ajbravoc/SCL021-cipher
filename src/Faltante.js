bd304b2ab328a670a3630178543650a442d44761
Anahí Bravo <108346279+Ajbravoc@users.noreply.github.com>
2023-01-23 22:59:43 -0300

    Creacion de medias queries

--- src/Faltante.js
+++ src/Faltante.js
@@ -1,16 +1,3 @@
-// Falta por desarrollar
-// Ingresar img ojo en los passwords inputs**
-// Uso de condicionales (if-else | switch | operador ternario)
-// Uso de bucles (for | for..in | for..of | while)
-// Uso de funciones (parámetros | argumentos | valor de retorno)
-//Agregar parámetros a las funciones para poder testearlas
-
 // Testing 
 // La descripción general de este proyecto no menciona qué pasaría con las letras minúsculas y otros 
-// caracteres (como espacios, puntuación, ñ, ...). El boilerplate incluye algunos tests (comentados en principio) 
-// que puedes usar como punto de partida para implementar el soporte para estos casos.
-//Cambiar todo en inglés 
-
-// libreria cipher 
-
-//node ejecuta mi codigo de js  e instalo npm (node package manager)
+// caracteres (como espacios, puntuación, ñ, ...)
